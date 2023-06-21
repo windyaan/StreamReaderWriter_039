a23f99c117ca2486414575f24252c8f982c3e25b
windyaan <windyanurmaretha@gmail.com>
2023-06-21 14:59:31 +0700

    Perulangan untuk menulis

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -13,5 +13,14 @@ int main() {
 
 	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
-
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukkan karekter q
+		if (baris == "q") break;
+		//menulis dan memasukkan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
 }
