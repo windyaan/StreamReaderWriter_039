bbb6c90cb0cfcaa7d44bc0843c140fe1291537f6
Windya Nurmaretha <126752841+windyaan@users.noreply.github.com>
2023-06-21 16:09:05 +0700

    project2

---
+++ Project2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <exception>
+//untuk obyek exception yang akan digunakan
+#include <array>
+//untuk obyek array yang akan kita gunakan
+using namespace std;
+int main()
+{
+	cout << "Awal Program" << endl; //penanda 1:...
+	try {
+		array<int, 3> data = { 3, 5, 7 };
+		//pesan array integer 3 elemen
+		cout << data.at(5) << endl;
+		//memanggil array element ke 5
+	}
+	catch (exception& e) {
+		//penangkap menggunakan obyek exception
+		cout << e.what() << endl;
+		/*akan dieksekusi karena array data hanya memiliki 3 element*/
+	}
+	cout << "Baris Program Yang terakhir" << endl;
+	/*penanda 2: bahwa program yang berjalan tanpa berhenti meskipun terjadi kesalahan*/
+	return 0;
+}
